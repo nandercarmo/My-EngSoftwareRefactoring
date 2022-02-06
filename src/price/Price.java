47450db9c2bbbedbab5cc14f27cc164e02c44d4a
NanderSantos <nandermgsdp@gmail.com>
2022-02-05 22:36:35 -0300

    Commit 14

--- src/price/Price.java
+++ src/price/Price.java
@@ -1,8 +1,19 @@
 package src.price;
 
+import src.movie.Movie;
+
 public abstract class Price {
 
 	public abstract int getPriceCode();
 
 	public abstract double getCharge(int daysRented);
+
+	public int getFrequentRenterPoints(int daysRented) {
+		if ((this.getPriceCode() == Movie.NEW_RELEASE) &&
+				daysRented > 1) {
+			return 2;
+		}
+
+		return 1;
+	}
 }
