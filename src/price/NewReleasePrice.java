a0ff1fd027181a30bafa6be901b83dc483cc284f
NanderSantos <nandermgsdp@gmail.com>
2022-02-05 22:35:01 -0300

    Commit 13

--- src/price/NewReleasePrice.java
+++ src/price/NewReleasePrice.java
@@ -7,5 +7,10 @@ public class NewReleasePrice extends Price {
 	@Override
 	public int getPriceCode() {
 		return Movie.NEW_RELEASE;
+	}
+
+	@Override
+	public double getCharge(int daysRented) {
+		return daysRented * 3;
 	}	
 }
